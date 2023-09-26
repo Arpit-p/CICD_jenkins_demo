58c4ee0026ea08c3373523da7566b1d47e0d169e
Arpit Parmar <aparmar5911@gmail.com>
2023-09-26 11:51:05 +0530

    initial commit

---
+++ before_start.sh
@@ -0,0 +1,6 @@
+cd /node_app
+# Install Node.js dependencies using npm
+npm install
+
+# Exit with a success status
+exit 0
---
+++ start_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the application directory
+cd /node_app
+
+# Start your Node.js application using npm or another command
+npm start
+# Exit with a success status
+exit 0
+
